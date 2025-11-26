43153b0a01c4d3f7b26ff5be8a0caa6d40d7aabe
Floremila <flormequi@gmail.com>
2025-11-26 16:54:41 +0100

    first commit

---
+++ src/main/java/se/floremila/checkgo/CheckgoApplication.java
@@ -0,0 +1,13 @@
+package se.floremila.checkgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckgoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckgoApplication.class, args);
+    }
+
+}
